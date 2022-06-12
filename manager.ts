824907237174fb3bd0204c373312149aa46f3ee4
Sjors van Holst <sjorsvanholst@gmail.com>
2022-06-12 17:34:45 +0200

    :poop: "Fixed" server

--- manager.ts
+++ manager.ts
@@ -6,7 +6,6 @@ interface Position {
 
 class Manager {
   clients: WebSocket[] = [];
-  positions: Position[] = [];
 
   addClient(client: WebSocket) {
     this.clients.push(client);
@@ -14,26 +13,9 @@ class Manager {
   }
 
   parseUpdate(data: string) {
-    const position: Position = JSON.parse(data);
-
-    // Push or replace the position in the positions array based on uuid
-    const index = this.positions.findIndex((p) => p.uuid === position.uuid);
-
-    if (index === -1) {
-      this.positions.push(position);
-    } else {
-      this.positions[index] = position;
-    }
-
-    this.sendPositions();
-  }
-
-  sendPositions() {
-    const body = JSON.stringify(this.positions);
-
     this.clients.forEach((client) => { 
       if (client.readyState === WebSocket.OPEN) {
-        client.send(body);
+        client.send(data);
       }
     });
   }
