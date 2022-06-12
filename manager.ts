42cf636d45f2b6074f8e1507d346346141fa99db
Sjors van Holst <sjorsvanholst@gmail.com>
2022-06-12 14:32:19 +0200

    :poop: Added temporary logic

--- manager.ts
+++ manager.ts
@@ -1,23 +1,41 @@
+interface Position {
+  x: number;
+  y: number;
+  uuid: string;
+}
+
 class Manager {
   clients: WebSocket[] = [];
-
-  constructor() {
-    setInterval(() => {
-      this.clients.forEach((client) => {
-        const body = JSON.stringify({
-          x: Math.random(),
-          y: Math.random(),
-        });
-
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(body);
-        }
-      });
-    }, 1000);
-  }
+  positions: Position[] = [];
 
   addClient(client: WebSocket) {
     this.clients.push(client);
+    client.onmessage = (event) => this.parseUpdate(event.data);
+  }
+
+  parseUpdate(data: string) {
+    const position: Position = JSON.parse(data);
+
+    // Push or replace the position in the positions array based on uuid
+    const index = this.positions.findIndex((p) => p.uuid === position.uuid);
+
+    if (index === -1) {
+      this.positions.push(position);
+    } else {
+      this.positions[index] = position;
+    }
+
+    this.sendPositions();
+  }
+
+  sendPositions() {
+    const body = JSON.stringify(this.positions);
+
+    this.clients.forEach((client) => { 
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(body);
+      }
+    });
   }
 }
 
